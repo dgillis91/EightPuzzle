d246857105b779003420334328a777c3ad70a5c1
dgillis91 <dgillis91@gmail.com>
2019-03-06 00:05:43 -0600

    modified copy_and_swap to use deepcopy so it can scale to refs

--- ListUtil.py
+++ ListUtil.py
@@ -5,10 +5,12 @@ Created on Mon Mar  4 08:49:43 2019
 @author: dgill
 """
 
+from copy import deepcopy
+
 def swap(index_one, index_two, lst):
     lst[index_one], lst[index_two] = lst[index_two], lst[index_one]
 
 def copy_and_swap(index_one, index_two, lst):
-    new_list = [x for x in lst]
+    new_list = deepcopy(lst)
     swap(index_one, index_two, new_list)
     return new_list
